09df99218f9b9999df11636ae7c03803ca4e58f4
avni <avni@hellorosey.com>
2019-07-26 15:44:56 +0200

    character card wiring complete, now onto styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard(props) {
+  return (
+    <span>
+      <img className="character-image" src={props.image} alt="" />
+      <h2 className="character-name">{props.name}</h2>
+      <h4 className="character-species">{props.species}</h4>
+      <p className="character-origin">{props.origin}</p>
+    </span>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+export default function CharacterList(props) {
+  const [characters, setCharacters] = useState([]);
+  console.log(characters);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        console.log(response.data.results);
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
+  }, []);
+  console.log(characters);
 
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+  return (
+    <section className="character-list grid-view">
+      {characters.map(character => (
+        <CharacterCard
+          key={character.id}
+          character={character}
+          name={character.name}
+          species={character.species}
+          origin={character.origin.name}
+          image={character.image}
+        />
+      ))}
     </section>
-
+  );
 }
