71c6246e75708185fd88d8cd4b74052965f6d4e6
avni <avni@hellorosey.com>
2019-07-26 18:04:38 +0200

    MVP complete

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,13 +3,15 @@ import { Card } from "semantic-ui-react";
 
 export default function CharacterCard(props) {
   return (
-    <Card>
-      <span className="character-list">
+    <Card className="character-list">
+      <span>
         <img className="character-image" src={props.image} alt="" />
-        <h2 className="character-name">{props.name}</h2>
-        <p className="character-status">{props.status}</p>
-        <p className="character-species">Species: {props.species}</p>
-        <p className="character-origin">Origin: {props.origin}</p>
+        <div className="character-info">
+          <h2 className="character-name">{props.name}</h2>
+          <p className="character-status">{props.status}</p>
+          <p className="character-species">Species: {props.species}</p>
+          <p className="character-origin">Origin: {props.origin}</p>
+        </div>
       </span>
     </Card>
   );
